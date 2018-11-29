25b26d7d4f3e43db26f343b6a7c74670f0653573
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-11-29 14:02:43 -0200

    Updates

---
+++ precache-manifest.cd8e8b5bd980fbc2c0f8402430ed476b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website-kune/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "51dcb41d177be9831f71",
+    "url": "/website-kune/static/js/runtime~main.51dcb41d.js"
+  },
+  {
+    "revision": "f5542e9d9f1d5a79647e",
+    "url": "/website-kune/static/js/main.f5542e9d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/website-kune/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f5542e9d9f1d5a79647e",
+    "url": "/website-kune/static/css/main.bbb337ed.chunk.css"
+  },
+  {
+    "revision": "16344c625891ea86351563c503a44be4",
+    "url": "/website-kune/index.html"
+  }
+];
