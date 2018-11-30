31d4ab62817f4fc94272ce5a9ae96053717961e8
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-11-30 06:46:35 -0200

    call tweaks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/Header';
+import Header from './components/Header';
+import Call from './components/Call'
+import Donate from './components/Donate'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="home" className="App">
         <Header />
+        <Call />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -22,6 +25,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Donate />
       </div>
     );
   }
