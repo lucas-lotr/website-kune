ff277f5bff47fe66210f5df738779111ddfc1ec3
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-11-30 07:42:24 -0200

    Collaborate

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Call from './components/Call'
-import Donate from './components/Donate'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Call from "./components/Call";
+import Donate from "./components/Donate";
+import Collaborate from "./components/Collaborate";
 
 class App extends Component {
-  render() {
-    return (
-      <div id="home" className="App">
-        <Header />
-        <Call />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Donate />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="home" className="App">
+                <Header />
+                <Call />
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                <Donate />
+                <Collaborate />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Collaborate/index.js
+++ src/components/Collaborate/index.js
@@ -1,2 +1,52 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import { Col, Row, Button } from "react-bootstrap";
+
+const Collaborate = props => {
+    return (
+        <Row id="planos">
+            <Col
+                id="col2"
+                md={6}
+                mdOffset={3}
+                lg={6}
+                lgOffset={3}
+                xs={10}
+                xsOffset={1}
+            >
+                <h2>
+                    <strong>SEJA UM AMIGO FIEL KUNÉ!</strong>
+                </h2>
+                <p>
+                    O Amigo Fiel Kuné é um programa de doacao mensal onde voce
+                    pode doar o valor desejado mensalmente e nos ajudar a
+                    treinar e entregar cada vez mais cães para pessoas que
+                    precisam.
+                </p>
+                <Row>
+                    <a href="https://pag.ae/7UjChYUm2">
+                        <Button>
+                            <strong>R$20</strong>
+                        </Button>
+                    </a>
+                    <a href="https://pag.ae/7UjCiRHg6">
+                        <Button>
+                            <strong>R$50</strong>
+                        </Button>
+                    </a>
+                    <a href="https://pag.ae/7UjCjGjM2">
+                        <Button>
+                            <strong>R$70</strong>
+                        </Button>
+                    </a>
+                    <a href="https://pag.ae/7UjCj-J-n">
+                        <Button>
+                            <strong>R$100</strong>
+                        </Button>
+                    </a>
+                </Row>
+            </Col>
+        </Row>
+    );
+};
+export default Collaborate;
