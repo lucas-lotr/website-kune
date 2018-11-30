3da9129cd2901fdb4435959141982e0b481f2290
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-11-30 07:42:31 -0200

    Call styling

--- src/components/Call/index.js
+++ src/components/Call/index.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import './style.css';
-import {
-    Jumbotron,
-    Col,
-    Row,
-    Button
+import React from "react";
+import "./style.css";
+import { Jumbotron, Col, Row, Button } from "react-bootstrap";
 
-} from 'react-bootstrap'
-
-
-const Call = (props) => {
+const Call = props => {
     return (
-        <Jumbotron>
+        <Jumbotron id="jumbo">
             <Row>
                 <br />
-                <Col xs={10} xsOffset={1} md={4} mdOffset={2} lg={4} lgOffset={2}>
-                    <p className="call" >
-                        Treinamos cães de suporte para pessoas com deficiências motoras,
-                        auditivas e para crianças no espectro autista.
-                        </p>
-                    <a href="#colabore"><Button block><strong>COLABORE!</strong></Button></a>
+                <Col
+                    xs={10}
+                    xsOffset={1}
+                    md={4}
+                    mdOffset={2}
+                    lg={4}
+                    lgOffset={2}
+                >
+                    <p className="call">
+                        Treinamos cães de suporte para pessoas com deficiências
+                        motoras, auditivas e para crianças no espectro autista.
+                    </p>
+                    <a href="#colabore">
+                        <Button block>
+                            <strong>COLABORE!</strong>
+                        </Button>
+                    </a>
                 </Col>
             </Row>
         </Jumbotron>
     );
-}
+};
 
 export default Call;
