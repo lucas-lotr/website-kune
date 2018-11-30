6502c1eecf5ed57cf6c8debab2389ff2ff7aaa4f
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-11-30 07:19:23 -0200

    Donate

--- src/components/Donate/index.js
+++ src/components/Donate/index.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import './style.css';
-import {
-    Col,
-    Row,
-    Button
+import React from "react";
+import "./style.css";
+import { Col, Row, Button } from "react-bootstrap";
 
-} from 'react-bootstrap'
-
-const Donate = (props) => {
+const Donate = props => {
     return (
-        <Row id='colabore'>
-            <Col md={6} mdOffset={3} lg={6} lgOffset={3} xs={10} xsOffset={1}>
-                <h3>COLABORE</h3>
+        <Row id="colabore">
+            <Col
+                id="col"
+                md={6}
+                mdOffset={3}
+                lg={6}
+                lgOffset={3}
+                xs={10}
+                xsOffset={1}
+            >
+                <h2>
+                    <strong>COLABORE</strong>
+                </h2>
                 <p>
-                    Atualmente precisamos investir em um filhote de labrador para ser nosso
-                    próximo cão de serviço. Além disso, a organização possui gastos
-                    recorrentes com as aulas de treinamento, cuidados com os cães,
-                    transporte, eventos, etc.
+                    Atualmente precisamos investir em um filhote de labrador
+                    para ser nosso próximo cão de serviço. Além disso, a
+                    organização possui gastos recorrentes com as aulas de
+                    treinamento, cuidados com os cães, transporte, eventos, etc.
                 </p>
+                <a href="https://pag.ae/7UnQ88FxL">
+                    <Button>
+                        <strong>DOE</strong>
+                    </Button>
+                </a>
             </Col>
         </Row>
     );
-}
+};
 export default Donate;
